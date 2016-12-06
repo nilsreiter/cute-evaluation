91e14c086c47fd7c1ca6d2ed97adcf617a2d7639
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-12-06 21:38:10 +0100

    re-enabled evaluations

--- run.sh
+++ run.sh
@@ -12,30 +12,30 @@ JAR_FILE=target/creta.cute.evaluation-0.0.1-SNAPSHOT.jar
 LABEL=dfki
 C=parzival
 CLASSES="PER LOC"
-#java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
+java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
 
 C=adorno
 CLASSES="PER WRK"
-#java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
+java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
 
 C=bundestagsdebatten
 CLASSES="PER LOC ORG WRK"
-#java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
+java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
 
 #### Sarah
 
 LABEL=ims
 C=parzival
 CLASSES="PER LOC ORG WRK EVT CNC"
-#java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
+java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
 
 C=adorno
 CLASSES="PER WRK ORG EVT CNC LOC"
-#java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
+java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
 
 C=bundestagsdebatten
 CLASSES="PER WRK ORG EVT CNC LOC"
-#java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
+java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
 
 #### Nils
 
