80d74007c0b788667d492b140e979b719b5b16b7
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-12-06 18:38:35 +0100

    script to run all evaluation code

---
+++ run.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+GOLD=/Users/reiterns/Documents/CRETA/cute/gold/xmi
+SILVER_BASE_DIR=/Users/reiterns/Documents/CRETA/cute/silver
+OUTPUT_BASE_DIR=/Users/reiterns/Documents/CRETA/cute/results
+
+JAR_FILE=target/creta.cute.evaluation-0.0.1-SNAPSHOT.jar
+
+LABEL=dfki
+
+C=parzival
+CLASSES="PER LOC"
+java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
+
+C=adorno
+CLASSES="PER WRK"
+java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
+
+C=bundestagsdebatten
+CLASSES="PER LOC ORG WRK"
+java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
+
+
+
+LABEL=ims
+C=parzival
+CLASSES="PER LOC ORG WRK EVT CNC"
+java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
+
+C=adorno
+CLASSES="PER WRK ORG EVT CNC LOC"
+java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
+
+C=bundestagsdebatten
+CLASSES="PER WRK ORG EVT CNC LOC"
+java -jar $JAR_FILE --gold $GOLD --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
+
+
+#### BASELINE
+
+LABEL=baseline-ner
+C=parzival
+CLASSES="PER LOC ORG WRK EVT CNC"
+java -jar $JAR_FILE --gold $GOLD --format xmi --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
+
+C=adorno
+CLASSES="PER WRK ORG EVT CNC LOC"
+java -jar $JAR_FILE --gold $GOLD --format xmi --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
+
+C=bundestagsdebatten
+CLASSES="PER WRK ORG EVT CNC LOC"
+java -jar $JAR_FILE --gold $GOLD --format xmi --label $LABEL_$C --silver $SILVER_BASE_DIR/$LABEL/$C --output $OUTPUT_BASE_DIR/$LABEL/$C --classes $CLASSES
