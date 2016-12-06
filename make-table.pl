4c43bc05ff3173bfb6c7f2af1386bf08579dfd3f
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2016-12-06 18:38:59 +0100

    script to make a per-category table

---
+++ make-table.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use warnings;
+
+my $DIR="/Users/reiterns/Documents/CRETA/cute/results";
+my @corpora = ("bundestagsdebatten", "adorno", "parzival");
+my @systems = ("dfki", "ims", "baseline-ner");
+
+my $tag = shift;
+my $column = shift; #2; # 2 precision, 3 recall
+
+printf("%20s\t","corpus");
+print join("\t", @systems)."\n";
+
+for my $c (@corpora) {
+  printf("%20s\t", $c);
+  for my $system (@systems) {
+    open(FH, "cat $DIR/$system/$c/$c.txt | grep $tag |");
+    while(<FH>) {
+      chomp;
+      my @l = split;
+      printf("%5.2f\t", $l[$column]*100);
+    }
+    close FH;
+  }
+  print "\n";
+}
